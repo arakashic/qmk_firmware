2921806a1cb9f31fd41269d550ee6bb26718499a
Yanfei Guo <yguo@anl.gov>
2021-05-01 19:05:59 -0500

    protok: tweak tap-hold configuration for quick typing

--- keyboards/protok/config.h
+++ keyboards/protok/config.h
@@ -64,8 +64,8 @@
 
 /* configurations for mod-tap keys */
 /* #define TAPPING_TERM 200 */
-#define PERMISSIVE_HOLD
-/* #define IGNORE_MOD_TAP_INTERRUPT */
+/* #define PERMISSIVE_HOLD */
+#define IGNORE_MOD_TAP_INTERRUPT
 /* #define TAPPING_FORCE_HOLD */
 
 #endif
