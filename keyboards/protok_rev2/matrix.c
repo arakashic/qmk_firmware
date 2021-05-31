969691bb30c5ded5a0a87ef4d9a5be5ab1e76c1e
Yanfei Guo <yguo@anl.gov>
2021-05-30 23:09:38 -0500

    protok-v2: add custom matrix for io mux

---
+++ keyboards/protok_rev2/matrix.c
@@ -0,0 +1,115 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "util.h"
+#include "matrix.h"
+#include "debounce.h"
+#include "quantum.h"
+
+static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
+static const pin_t col_addr_pins[MATRIX_COL_ADDR_BITS] = MATRIX_COL_ADDR_PINS;
+
+/* matrix state(1:on, 0:off) */
+extern matrix_row_t raw_matrix[MATRIX_ROWS];  // raw values
+extern matrix_row_t matrix[MATRIX_ROWS];      // debounced values
+
+static inline void setPinOutput_writeLow(pin_t pin) {
+    ATOMIC_BLOCK_FORCEON {
+        setPinOutput(pin);
+        writePinLow(pin);
+    }
+}
+
+static inline void setPinInputHigh_atomic(pin_t pin) {
+    ATOMIC_BLOCK_FORCEON { setPinInputHigh(pin); }
+}
+
+// matrix code
+
+static void select_row(uint8_t row) { setPinOutput_writeLow(row_pins[row]); }
+
+static void unselect_row(uint8_t row) { setPinInputHigh_atomic(row_pins[row]); }
+
+static void unselect_rows(void) {
+    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
+        setPinInputHigh_atomic(row_pins[x]);
+    }
+}
+
+static inline void setColPinAddr(uint8_t addr) {
+    ATOMIC_BLOCK_FORCEON {
+        uint8_t mask = 0x1;
+        for (uint8_t pin_bit = 0; pin_bit < MATRIX_COL_ADDR_BITS; pin_bit++) {
+            writePin(col_addr_pins[pin_bit], addr & mask);
+            mask = (mask << 1);
+        }
+    }
+}
+
+static void init_pins(void) {
+    unselect_rows();
+    for (uint8_t x = 0; x < MATRIX_COL_ADDR_BITS; x++) {
+        setPinOutput(col_addr_pins[x]);
+    }
+    setPinInputHigh_atomic(MATRIX_COL_SIG_PIN);
+}
+
+static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
+    // Start with a clear matrix row
+    matrix_row_t current_row_value = 0;
+
+    // Select row
+    select_row(current_row);
+    matrix_output_select_delay();
+
+    // For each col...
+    for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
+        // Select the col pin to read (active low)
+        setColPinAddr(col_index);
+        // Wait for propagation
+        wait_us(1);
+        uint8_t pin_state = readPin(MATRIX_COL_SIG_PIN);
+
+        // Populate the matrix row with the state of the col pin
+        current_row_value |= pin_state ? 0 : (MATRIX_ROW_SHIFTER << col_index);
+    }
+
+    // Unselect row
+    unselect_row(current_row);
+    matrix_output_unselect_delay();  // wait for all Col signals to go HIGH
+
+    // If the row has changed, store the row and return the changed flag.
+    if (current_matrix[current_row] != current_row_value) {
+        current_matrix[current_row] = current_row_value;
+        return true;
+    }
+    return false;
+}
+
+void matrix_init_custom(void) {
+    // initialize key pins
+    init_pins();
+
+    // initialize matrix state: all keys off
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        raw_matrix[i] = 0;
+        matrix[i]     = 0;
+    }
+
+    debounce_init(MATRIX_ROWS);
+
+    matrix_init_quantum();
+}
+
+uint8_t matrix_scan_custom(void) {
+    bool changed = false;
+
+    // Set row, read cols
+    for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
+        changed |= read_cols_on_row(raw_matrix, current_row);
+    }
+
+    debounce(raw_matrix, matrix, MATRIX_ROWS, changed);
+
+    matrix_scan_quantum();
+    return (uint8_t)changed;
+}
