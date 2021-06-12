4e42e9bc56d76e2a91aa6209717c196a9e565c35
Yanfei Guo <yguo@anl.gov>
2021-06-12 00:30:14 -0500

    protok-v2: add initial library for MCP23S17

--- keyboards/protok_rev2/halconf.h
+++ keyboards/protok_rev2/halconf.h
@@ -17,3 +17,10 @@
 #include_next "halconf.h"
 
 /* #define HAL_USE_I2C TRUE */
+#undef HAL_USE_SPI
+#define HAL_USE_SPI TRUE
+#undef SPI_USE_WAIT
+#define SPI_USE_WAIT TRUE
+#undef SPI_SELECT_MODE
+#define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
+
