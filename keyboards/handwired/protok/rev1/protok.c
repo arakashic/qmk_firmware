55f4c6e63d0b9152987949fc7e67324201fd887b
Yanfei Guo <yguo@anl.gov>
2021-08-22 12:24:05 -0500

    protok: add rev1 into handwired directory

---
+++ keyboards/handwired/protok/rev1/protok.c
@@ -0,0 +1,16 @@
+#include "protok.h"
+
+/* extern inline void protok_caps_led_on(void); */
+/* extern inline void protok_caps_led_off(void); */
+
+void led_set_kb(uint8_t usb_led) {
+    // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+
+    /* if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) { */
+    /*     protok_caps_led_on(); */
+    /* } else { */
+    /*     protok_caps_led_off(); */
+    /* } */
+
+    /* led_set_user(usb_led); */
+}
