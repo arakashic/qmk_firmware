c4592e12a94806a97ce221475491e438c60b7954
Yanfei Guo <yguo@anl.gov>
2021-10-09 11:13:19 -0500

    protok: update rev1

--- keyboards/handwired/protok/rev1/rules.mk
+++ keyboards/handwired/protok/rev1/rules.mk
@@ -29,5 +29,7 @@ NKRO_ENABLE = yes			# USB Nkey Rollover - if this doesn't work, see here: https:
 # UNICODE_ENABLE = YES		# Unicode
 # BLUETOOTH_ENABLE = yes	# Enable Bluetooth with the Adafruit EZ-Key HID
 # TERMINAL_ENABLE = yes
-# OLED_DRIVER_ENABLE = yes
+OLED_ENABLE = yes
+
+SRC += i2c_master.c
 
