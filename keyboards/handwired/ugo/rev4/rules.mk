9761a49063146ac3b19dab492f3208a0c71c411f
Yanfei Guo <yguo@anl.gov>
2023-09-13 16:27:52 -0500

    Add flash based EEPROM

--- keyboards/handwired/ugo/rev4/rules.mk
+++ keyboards/handwired/ugo/rev4/rules.mk
@@ -1,6 +1,10 @@
 # This file intentionally left blank
 CUSTOM_MATRIX=lite
 
+EEPROM_DRIVER=wear_leveling
+WEAR_LEVELING_DRIVER=spi_flash
+FLASH_DRIVER = spi
+
 QUANTUM_LIB_SRC += spi_master.c
 
 SRC += matrix.c
