0c05874e9d7c0b5ec4f14a1920def15be7a931c0
Yanfei Guo <yguo@anl.gov>
2023-09-25 14:43:20 -0500

    protok: add programmable button

--- keyboards/handwired/ugo/protok/keymaps/rev3/keymap.c
+++ keyboards/handwired/ugo/protok/keymaps/rev3/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_HOME  , LT(L_NAV, KC_TAB)           , KC_Q   , KC_W    , KC_E    , KC_R    , KC_T   , KC_Y          , KC_U    , KC_I    , KC_O    , KC_P    , KC_LBRC , KC_RBRC , KC_DEL  ,
            KC_END   , LCTL_T(KC_ESC)              , KC_A   , KC_S    , KC_D    , KC_F    , KC_G   , KC_H          , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT , KC_BSLS , MO(L_FUN),
            XXXXXXX  ,                     SC_LSPO , KC_Z   , KC_X    , KC_C    , KC_V    , KC_B   , KC_N          , KC_M    , KC_COMM , KC_DOT  , KC_SLSH ,           SC_RSPC , KC_PGUP   ,
-           MO(L_CMD), XXXXXXX           , XXXXXXX , KC_HYPR, KC_LGUI , KC_LALT , LT(L_SYM, KC_BSPC) , RSFT_T(KC_SPC) , KC_ENT, KC_RALT, KC_RGUI , KC_LCTL , XXXXXXX , KC_UP   , KC_PGDN ,
+           MO(L_CMD),     PB_1          ,    PB_2 , KC_HYPR, KC_LGUI , KC_LALT , LT(L_SYM, KC_BSPC) , RSFT_T(KC_SPC) , KC_ENT, KC_RALT, KC_RGUI , KC_LCTL , XXXXXXX , KC_UP   , KC_PGDN ,
                                                                                                                                                             KC_LEFT , KC_DOWN , KC_RGHT),
     /* Game layer PC */
     LAYOUT_gen2(
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_ACL1, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
            KC_ACL0, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
            XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,
-           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX         , XXXXXXX, XXXXXXX, XXXXXXX, KC_7   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+           XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX         , XXXXXXX, XXXXXXX, XXXXXXX, KC_7   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                                                                                                                 XXXXXXX, XXXXXXX, XXXXXXX),
 };
 
