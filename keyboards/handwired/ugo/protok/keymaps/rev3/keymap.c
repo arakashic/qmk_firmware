2aa7669950373a37f14720425b2b109212c68e82
Yanfei Guo <yguo@anl.gov>
2023-09-28 14:58:26 -0500

    protok: reformat mapping

--- keyboards/handwired/ugo/protok/keymaps/rev3/keymap.c
+++ keyboards/handwired/ugo/protok/keymaps/rev3/keymap.c
@@ -22,6 +22,11 @@ enum layer_names {
     L_CMD
 };
 
+#define PK_BSPC LT(L_SYM, KC_BSPC)
+#define PK_SPC  RSFT_T(KC_SPC)
+#define PK_LCTL LCTL_T(KC_ESC)
+#define PK_TAB  LT(L_NAV, KC_TAB)
+
 os_variant_t os = OS_UNSURE;
 
 // clang-format off
@@ -29,22 +34,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* base layer PC */
     LAYOUT_gen2(
-           KC_MUTE                      , KC_F1   , KC_F2  , KC_F3   , KC_F4   , KC_F5   , KC_F6  , KC_F7         , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_CAPS , KC_INS  ,
-           KC_DEL   , KC_GRV            , KC_1    , KC_2   , KC_3    , KC_4    , KC_5    , KC_6   , KC_7          , KC_8    , KC_9    , KC_0    , KC_MINS , KC_EQL  , KC_HOME , KC_END  ,
-           KC_HOME  , LT(L_NAV, KC_TAB)           , KC_Q   , KC_W    , KC_E    , KC_R    , KC_T   , KC_Y          , KC_U    , KC_I    , KC_O    , KC_P    , KC_LBRC , KC_RBRC , KC_DEL  ,
-           KC_END   , LCTL_T(KC_ESC)              , KC_A   , KC_S    , KC_D    , KC_F    , KC_G   , KC_H          , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT , KC_BSLS , MO(L_FUN),
-           XXXXXXX  ,                     SC_LSPO , KC_Z   , KC_X    , KC_C    , KC_V    , KC_B   , KC_N          , KC_M    , KC_COMM , KC_DOT  , KC_SLSH ,           SC_RSPC , KC_PGUP   ,
-           MO(L_CMD),     PB_1          ,    PB_2 , KC_HYPR, KC_LGUI , KC_LALT , LT(L_SYM, KC_BSPC) , RSFT_T(KC_SPC) , KC_ENT, KC_RALT, KC_RGUI , KC_LCTL , XXXXXXX , KC_UP   , KC_PGDN ,
-                                                                                                                                                            KC_LEFT , KC_DOWN , KC_RGHT),
+           KC_MUTE           , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_CAPS, KC_INS ,
+           KC_DEL   , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_HOME, KC_END ,
+           KC_HOME  , PK_TAB ,          KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_DEL ,
+           KC_END   , PK_LCTL,          KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_BSLS, MO(L_FUN),
+           XXXXXXX  ,          SC_LSPO, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,          SC_RSPC, KC_PGUP,
+           MO(L_CMD), PB_1   , PB_2   , KC_HYPR, KC_LGUI,          KC_LALT, PK_BSPC, PK_SPC , KC_ENT , KC_RALT, KC_RGUI, KC_LCTL, XXXXXXX, KC_UP  , KC_PGDN,
+                                                                                                                                  KC_LEFT, KC_DOWN, KC_RGHT),
     /* Game layer PC */
     LAYOUT_gen2(
-           KC_ESC                       , KC_F1   , KC_F2  , KC_F3   , KC_F4   , KC_F5   , KC_F6  , KC_F7         , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_CAPS , KC_INS  ,
-           KC_ESC   , KC_GRV            , KC_1    , KC_2   , KC_3    , KC_4    , KC_5    , KC_6   , KC_7          , KC_8    , KC_9    , KC_0    , KC_MINS , KC_EQL  , KC_HOME , KC_END  ,
-           KC_HOME  , KC_TAB                      , KC_Q   , KC_W    , KC_E    , KC_R    , KC_T   , KC_Y          , KC_U    , KC_I    , KC_O    , KC_P    , KC_LBRC , KC_RBRC , KC_BSPC ,
-           KC_END   , LCTL_T(KC_ESC)              , KC_A   , KC_S    , KC_D    , KC_F    , KC_G   , KC_H          , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT , KC_BSLS , MO(L_FUN),
-           KC_DEL   ,                     SC_LSPO , KC_Z   , KC_X    , KC_C    , KC_V    , KC_B   , KC_N          , KC_M    , KC_COMM , KC_DOT  , KC_SLSH ,           SC_RSPC , KC_PGUP ,
-           MO(L_CMD), XXXXXXX           , XXXXXXX , KC_HYPR, KC_LGUI           , KC_LALT , KC_SPC , KC_SPC        , KC_ENT  , KC_RALT , KC_RGUI , KC_LCTL , XXXXXXX , KC_UP   , KC_PGDN ,
-                                                                                                                                                            KC_LEFT , KC_DOWN , KC_RGHT),
+           KC_ESC            , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_CAPS, KC_INS ,
+           KC_ESC   , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_HOME, KC_END ,
+           KC_HOME  , KC_TAB ,          KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC,
+           KC_END   , PK_LCTL,          KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_BSLS, MO(L_FUN),
+           KC_DEL   ,          SC_LSPO, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,          SC_RSPC, KC_PGUP,
+           MO(L_CMD), XXXXXXX, XXXXXXX, KC_HYPR, KC_LGUI,          KC_LALT, KC_SPC , KC_SPC , KC_ENT , KC_RALT, KC_RGUI, KC_LCTL, XXXXXXX, KC_UP  , KC_PGDN,
+                                                                                                                                  KC_LEFT, KC_DOWN, KC_RGHT),
     /* additional func */
     LAYOUT_gen2(
            _______,          KC_PWR , KC_SLEP, KC_WAKE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
