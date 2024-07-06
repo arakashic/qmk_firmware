50a9600510592e36ae0a61777846cf27907fc297
Yanfei Guo <yanf.guo@gmail.com>
2024-07-05 21:42:34 -0500

    protok: rename to rev3

--- keyboards/handwired/ugo/rev3/config.h
+++ keyboards/handwired/ugo/rev3/config.h
@@ -41,7 +41,7 @@
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 
 #define OLED_DISPLAY_128X64
-#define OLED_FONT_H "keyboards/handwired/ugo/protok/keymaps/rev3/glcdfont.c"
+#define OLED_FONT_H "keyboards/handwired/ugo/rev3/keymaps/rev3/glcdfont.c"
 
 /* configurations for mod-tap keys */
 /* #define TAPPING_TERM 200 */
